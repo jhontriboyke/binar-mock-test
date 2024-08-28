7406368b545e1f7b6703b3c0620c3c2ccff640d3
Jhontri Boyke <51961172+jhontriboyke@users.noreply.github.com>
2024-08-28 14:10:07 +0700

    add(): landing page

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const responseFormat = require("./middlewares/response-format.middleware");
 app.use(responseFormat);
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send(`
+      <div>
+        <h1>Welcome to Homepage</h1>
+        <p>Go to <a href="./auth/register">register</a> to create new user</p>
+        <p>Go to <a href="./auth/login">login</a> to login</p>
+        <p>Go to <a href="./todos">todos</a> to access todos</p>
+      </div>
+    `);
+});
+
 const TODO_ROUTES = require("./routes/todo.routes");
 const AUTH_ROUTES = require("./routes/auth.routes");
 app.use("/auth", AUTH_ROUTES);
